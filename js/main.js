2c53f8ff304ec99abc8c00c8fb267b8ec7ccfd5e
Adler Luders <adlerluders@gmail.com>
2020-03-21 22:24:41 -0400

    Worked on displaying answers

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 const UIElements = {
   tabs: ".features-tab",
   titles: ".features-tab__title",
-  contents: ".features-tab__content"
+  contents: ".features-tab__content",
+  questions: ".question-header",
+  answers: ".answer"
 };
 
 const tabs = document.querySelectorAll(UIElements.tabs);
 const titles = document.querySelectorAll(UIElements.titles);
 const contents = document.querySelectorAll(UIElements.contents);
+const questions = document.querySelectorAll(UIElements.questions);
+const answers = document.querySelectorAll(UIElements.answers);
 
 function showContent() {
   removeActive();
@@ -20,6 +24,17 @@ function showContent() {
   messages.style.display = "grid";
 }
 
+function displayAnswer() {
+  hideAnswers();
+
+  const questionAnswer = document.getElementById(`${this.id}__answer`);
+  const arrow = this.childNodes[3];
+
+  questionAnswer.style.display = "block";
+
+  arrow.setAttribute("src", "images/icon-arrow-close.svg");
+}
+
 function hideContents() {
   contents.forEach(content => (content.style.display = "none"));
 }
@@ -28,9 +43,15 @@ function removeActive() {
   titles.forEach(title => title.classList.remove("active-tab"));
 }
 
-// console.log(tabs);
-
-// console.log(contents);
+function hideAnswers() {
+  answers.forEach(answer => (answer.style.display = "none"));
+}
 
 // Event Listeners
 tabs.forEach(tab => tab.addEventListener("click", showContent));
+
+questions.forEach(question =>
+  question.addEventListener("click", displayAnswer)
+);
+
+window.addEventListener("load", hideAnswers);
