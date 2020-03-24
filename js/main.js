b1e8b371542e18422931002d35f4ee79dfa6e322
Adler Luders <adlerluders@gmail.com>
2020-03-23 21:53:26 -0400

    Final Commit before sumbission

--- js/main.js
+++ js/main.js
@@ -5,18 +5,26 @@ const UIElements = {
   questions: ".question-header",
   answers: ".answer",
   email: "#email",
-  form: ".contact-form"
+  form: ".contact-form",
+  invalid: ".invalid-email",
+  errorImg: "#email + img",
+  togglerOpen: ".toggler",
+  togglerClose: ".close-mobile-nav",
+  mobileNav: ".mobile-nav"
 };
 
-const tabs = document.querySelectorAll(UIElements.tabs);
-const titles = document.querySelectorAll(UIElements.titles);
-const contents = document.querySelectorAll(UIElements.contents);
-const questions = document.querySelectorAll(UIElements.questions);
-const answers = document.querySelectorAll(UIElements.answers);
-const email = document.querySelector(UIElements.email);
-const form = document.querySelector(UIElements.form);
-
-console.log(form);
+const tabs = document.querySelectorAll(UIElements.tabs),
+  titles = document.querySelectorAll(UIElements.titles),
+  contents = document.querySelectorAll(UIElements.contents),
+  questions = document.querySelectorAll(UIElements.questions),
+  answers = document.querySelectorAll(UIElements.answers),
+  email = document.querySelector(UIElements.email),
+  form = document.querySelector(UIElements.form),
+  invalid = document.querySelector(UIElements.invalid),
+  errorImg = document.querySelector(UIElements.errorImg),
+  togglerOpen = document.querySelector(UIElements.togglerOpen),
+  togglerClose = document.querySelector(UIElements.togglerClose),
+  mobileNav = document.querySelector(UIElements.mobileNav);
 
 function showContent() {
   removeActive();
@@ -45,7 +53,15 @@ function displayAnswer() {
 
 function validateEmail(e) {
   e.preventDefault();
-  console.log(e);
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,7})$/;
+
+  if (!re.test(email.value)) {
+    errorImg.style.display = "block";
+    invalid.style.display = "flex";
+  } else {
+    errorImg.style.display = "none";
+    invalid.style.display = "none";
+  }
 }
 
 function hideContents() {
@@ -56,6 +72,14 @@ function removeActive() {
   titles.forEach(title => title.classList.remove("active-tab"));
 }
 
+function openMenu() {
+  mobileNav.style.display = "block";
+}
+
+function closeMenu() {
+  mobileNav.style.display = "none";
+}
+
 // Event Listeners
 tabs.forEach(tab => tab.addEventListener("click", showContent));
 
@@ -64,3 +88,7 @@ questions.forEach(question =>
 );
 
 form.addEventListener("submit", validateEmail);
+
+togglerOpen.addEventListener("click", openMenu);
+
+togglerClose.addEventListener("click", closeMenu);
